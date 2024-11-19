273164566fe20f2aecb7aa0e63c92453579a1863
phanvanthuan06052004 <phanthuan652004@gmail.com>
2024-11-19 22:56:35 +0700

    upload footer

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './Banner'
 import BestSeller from './BestSeller'
 import Recomment from './Recomment'
+import News from './News'
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <BestSeller />
             <Recomment />
+            <News />
         </div>
     )
 }
