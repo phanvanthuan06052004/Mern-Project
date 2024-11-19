daef692a0973acfadb92fd3b8034d360e777afab
phanvanthuan06052004 <phanthuan652004@gmail.com>
2024-11-19 22:02:20 +0700

    Upload Recoment Ui

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import BestSeller from './BestSeller'
+import Recomment from './Recomment'
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <BestSeller />
+            <Recomment />
         </div>
     )
 }
